c88a86d90750e4776c6f82f99183eebce6e1d470
Pooja Yadav <poojayadav@Poojas-MacBook-Pro.local>
2025-12-17 13:05:32 +0530

    Fix build.sh with full node & npm path for Jenkins

--- build.sh
+++ build.sh
@@ -2,14 +2,12 @@
 set -e
 
 echo "Installing dependencies"
-/opt/homebrew/bin/npm install
+/opt/homebrew/bin/node /opt/homebrew/bin/npm install
 
 echo "Building React app"
-/opt/homebrew/bin/npm run build
+/opt/homebrew/bin/node /opt/homebrew/bin/npm run build
 
 echo "Building Docker image"
 docker build -t poojayadav253/react-app:latest .
-
-
 
 
